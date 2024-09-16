75651a7c6f7da3ddd343d98d108dcebb8fd5854a
Szymon Durak <superdurszlak@gmail.com>
2024-09-16 22:14:35 +0200

    Scroll PlantUML instead of resizing, move augmentor scripts to Jekyll

--- assets/js/fix-aria-hidden.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const anchors = document.querySelectorAll('a.anchor');
-    anchors.forEach(function(anchor) {
-        anchor.setAttribute('aria-hidden', 'false');
-        anchor.setAttribute('aria-label', anchor.parentElement.textContent)
-    });
-  });
