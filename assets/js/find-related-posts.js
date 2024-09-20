f6f981aafe797882ee15cdb0093e2950d8ddd650
Szymon Durak <superdurszlak@gmail.com>
2024-09-20 07:21:43 +0200

    WIP add script to generate further reading

--- assets/js/find-related-posts.js
+++ assets/js/find-related-posts.js
@@ -3,17 +3,51 @@
   const basePost = data.ref;
   const postTags = data.tags;
 
+  const relatedPosts = allResults()
+    .filter((result) => {
+      return basePost == result.ref;
+    })
+    .filter((result) => {
+      return matchesByTags(result.tags, postTags);
+    })
+    .map((result) => {
+      result.html;
+    });
 
-  var allResults = Object.keys(window.store).map((key) => {
-    return {
-      ref: key,
-      tags: window.store[key].tags,
-      html: window.store[key].html
-    };
-  });
+  const relatedPostsUl = document.getElementById('related-posts');
+  
+  relatedPostsUl.innerHTML = shuffleResults(relatedPosts)
+    .join("");
 
-  function findPostsByTag(tag) {
+  enableRelatedPosts();
 
+  function enableRelatedPosts() {
+    document.getElementById('related-posts-wrapper').style['display'] = 'block';
   }
-  // You can place any logic here
+
+  function matchesByTags(foundTags, relevantTags) {
+    const relevantTagsSet = new Set(relevantTags);
+    return foundTags.some((tag) => {
+      return relevantTagsSet.has(tag);
+    });
+  }
+
+  function allResults() {
+    return Object.keys(window.store).map((key) => {
+      return {
+        ref: key,
+        tags: window.store[key].tags,
+        html: window.store[key].html,
+      };
+    });
+  }
+
+  function shuffleResults(results) {
+    const copy = [...results];
+    for (let index = copy.length - 1; index > 0; index--) {
+      const swapIndex = Math.floor(Math.random() * (index + 1));
+      [copy[index], copy[swapIndex]] = [copy[swapIndex], copy[index]];
+    }
+    return copy;
+  }
 })();
