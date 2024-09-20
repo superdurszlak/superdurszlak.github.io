45df8090200c18cafb1e1f40567b23d8ab880301
Szymon Durak <superdurszlak@gmail.com>
2024-09-20 22:59:44 +0200

    Cross-links support (#33)
    - Each post is linked to previous and next
    - If JS is enabled, relevant posts with at least one common tag are randomly picked for further reading
    - Various stylesheet tweaks
    - Fixed bug in post search that led to effective exclusion of tags from search

---
+++ assets/js/find-related-posts.js
@@ -0,0 +1,59 @@
+(function () {
+  const data = window.store.current || {};
+  const basePost = data.ref || "";
+  const postTags = data.tags || [];
+
+  const relatedPosts = allResults()
+    .filter((result) => {
+      return basePost != result.ref;
+    })
+    .filter((result) => {
+      return matchesByTags(result.tags, postTags);
+    })
+    .map((result) => {
+      return result.html;
+    });
+
+
+  const relatedPostsUl = document.getElementById('related-posts');
+  
+  relatedPostsUl.innerHTML = shuffleResults(relatedPosts)
+    .slice(0, 3)
+    .join("");
+
+  enableRelatedPosts();
+
+  function enableRelatedPosts() {
+    document.getElementById('related-posts-wrapper').style['display'] = 'block';
+  }
+
+  function matchesByTags(foundTags, relevantTags) {
+    const relevantTagsSet = new Set(relevantTags);
+    console.log("Looking for matches: %s vs %s", foundTags, relevantTags);
+    const hasMatch = foundTags.some((tag) => {
+      return relevantTagsSet.has(tag);
+    });
+    console.log("Match result: %s", hasMatch);
+    return hasMatch;
+  }
+
+  function allResults() {
+    const posts = window.store.posts;
+    return Object.keys(posts).map((key) => {
+      return {
+        ref: key,
+        tags: posts[key].tags,
+        html: posts[key].html,
+      };
+    });
+  }
+
+  function shuffleResults(results) {
+    const copy = [...results];
+    for (let index = copy.length - 1; index > 0; index--) {
+      const swapIndex = Math.floor(Math.random() * (index + 1));
+      [copy[index], copy[swapIndex]] = [copy[swapIndex], copy[index]];
+    }
+    return copy;
+  }
+})();
