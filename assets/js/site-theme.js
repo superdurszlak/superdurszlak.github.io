504cf4af00e1445e0dcea9844a30f4f5d36febb6
Szymon Durak <superdurszlak@gmail.com>
2024-11-11 19:04:02 +0100

    Three basic modes, light dark and browser

--- assets/js/site-theme.js
+++ assets/js/site-theme.js
@@ -1,8 +1,16 @@
+const themeChoiceMapping = {
+  browser: getDefaultPreference,
+  light: () => "light",
+  dark: () => "dark",
+  "dark-contrast": () => "dark-contrast",
+  "light-contrast": () => "light-contrast",
+};
+
 const giscusThemeMapping = {
   light: "light",
   dark: "dark_dimmed",
   "dark-contrast": "dark_high_contrast",
-  "light-contrast": "light_high_contrast"
+  "light-contrast": "light_high_contrast",
 };
 
 const themeKey = "theme";
@@ -10,71 +18,72 @@ const themeKey = "theme";
 document.addEventListener("DOMContentLoaded", () => {
   const themeButtons = document.querySelectorAll(".theme-option");
 
-  const preSelected = localStorage.getItem(themeKey) || getDefaultPreference();
+  const preSelected = localStorage.getItem(themeKey) || "browser";
   setThemes(preSelected);
 
   themeButtons.forEach((button) => {
     button.addEventListener("change", (event) => setThemes(event.target.value));
   });
-
-  function setThemes(theme) {
-    localStorage.setItem(themeKey, theme)
-    const giscusTheme = giscusThemeMapping[theme];
-    setSiteTheme(theme);
-    setGiscusTheme(giscusTheme);
-  }
-
-  function setSiteTheme(theme) {
-    document.documentElement.setAttribute("data-theme", theme);
-  }
-
-  function setGiscusTheme(giscusTheme) {
-    if (!giscusTheme) return;
-
-    const iframe = document.querySelector("iframe.giscus-frame");
-
-    if (!iframe) return;
-
-    const message = {
-      giscus: {
-        setConfig: {
-          theme: giscusTheme,
-        },
-      },
-    };
-    iframe.contentWindow.postMessage(message, "https://giscus.app");
-    console.log("done setting theme to ", giscusTheme);
-  }
-
-  function getDefaultPreference() {
-    var mode = "light";
-
-    if (
-      window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches
-    ) {
-      mode = "dark";
-    }
-
-    if (highContrastHeuristicMatches()) {
-      mode += "-contrast";
-    }
-
-    return mode;
-  }
-
-  function browserDarkModeMatches() {}
-
-  function highContrastHeuristicMatches() {
-    const heuristicColor = "rgb(0, 0, 0)";
-    const div = document.createElement("div");
-    div.style.backgroundColor = heuristicColor;
-    document.body.appendChild(div);
-
-    const computedColor = window.getComputedStyle(div).backgroundColor;
-    document.body.removeChild(div);
-
-    // If the computed color has changed, it indicates the browser forces high contrast or other mode
-    return computedColor !== heuristicColor;
-  }
 });
+
+function setThemes(choice) {
+  const theme = (themeChoiceMapping[choice] || getDefaultPreference)();
+  localStorage.setItem(themeKey, theme);
+  const giscusTheme = giscusThemeMapping[theme];
+  setSiteTheme(theme);
+  setGiscusTheme(giscusTheme);
+}
+
+function setSiteTheme(theme) {
+  document.documentElement.setAttribute("data-theme", theme);
+}
+
+function setGiscusTheme(giscusTheme) {
+  if (!giscusTheme) return;
+
+  const iframe = document.querySelector("iframe.giscus-frame");
+
+  if (!iframe) return;
+
+  const message = {
+    giscus: {
+      setConfig: {
+        theme: giscusTheme,
+      },
+    },
+  };
+  iframe.contentWindow.postMessage(message, "https://giscus.app");
+  console.log("done setting theme to ", giscusTheme);
+}
+
+function getDefaultPreference() {
+  var mode = "light";
+
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    mode = "dark";
+  }
+
+  if (highContrastHeuristicMatches()) {
+    mode += "-contrast";
+  }
+
+  return mode;
+}
+
+function browserDarkModeMatches() {}
+
+function highContrastHeuristicMatches() {
+  const heuristicColor = "rgb(0, 0, 0)";
+  const div = document.createElement("div");
+  div.style.backgroundColor = heuristicColor;
+  document.body.appendChild(div);
+
+  const computedColor = window.getComputedStyle(div).backgroundColor;
+  document.body.removeChild(div);
+
+  // If the computed color has changed, it indicates the browser forces high contrast or other mode
+  return computedColor !== heuristicColor;
+}
