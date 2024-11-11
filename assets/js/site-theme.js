70528d95c5a08afbcf1f5f721c908bac001d81a9
Szymon Durak <superdurszlak@gmail.com>
2024-11-11 14:29:47 +0100

    In progress - mode preferences stored in browser and based on browser preferences

--- assets/js/site-theme.js
+++ assets/js/site-theme.js
@@ -2,20 +2,27 @@ const giscusThemeMapping = {
   light: "light",
   dark: "dark_dimmed",
   "dark-contrast": "dark_high_contrast",
-  "light-contrast": "light_high_contrast",
+  "light-contrast": "light_high_contrast"
 };
+
+const themeKey = "theme";
 
 document.addEventListener("DOMContentLoaded", () => {
   const themeButtons = document.querySelectorAll(".theme-option");
 
+  const preSelected = localStorage.getItem(themeKey) || getDefaultPreference();
+  setThemes(preSelected);
+
   themeButtons.forEach((button) => {
-    button.addEventListener("change", (event) => {
-      const theme = event.target.value;
-      const giscusTheme = giscusThemeMapping[theme];
-      setSiteTheme(theme);
-      setGiscusTheme(giscusTheme);
-    });
+    button.addEventListener("change", (event) => setThemes(event.target.value));
   });
+
+  function setThemes(theme) {
+    localStorage.setItem(themeKey, theme)
+    const giscusTheme = giscusThemeMapping[theme];
+    setSiteTheme(theme);
+    setGiscusTheme(giscusTheme);
+  }
 
   function setSiteTheme(theme) {
     document.documentElement.setAttribute("data-theme", theme);
@@ -37,5 +44,37 @@ document.addEventListener("DOMContentLoaded", () => {
     };
     iframe.contentWindow.postMessage(message, "https://giscus.app");
     console.log("done setting theme to ", giscusTheme);
+  }
+
+  function getDefaultPreference() {
+    var mode = "light";
+
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      mode = "dark";
+    }
+
+    if (highContrastHeuristicMatches()) {
+      mode += "contrast";
+    }
+
+    return mode;
+  }
+
+  function browserDarkModeMatches() {}
+
+  function highContrastHeuristicMatches() {
+    const heuristicColor = "rgb(0.25, 0.25, 0.25)";
+    const div = document.createElement("div");
+    div.style.backgroundColor = heuristicColor;
+    document.body.appendChild(div);
+
+    const computedColor = window.getComputedStyle(div).backgroundColor;
+    document.body.removeChild(div);
+
+    // If the computed color has changed, it indicates the browser forces high contrast or other mode
+    return computedColor !== heuristicColor;
   }
 });
